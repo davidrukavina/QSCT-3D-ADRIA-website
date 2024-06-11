e87b5e086c53e5855e2e1275cd90b64be688ba51
David Rukavina <drukavina@rgn.hr>
2024-06-11 11:06:16 +0200

    test

--- Homepage.py
+++ Homepage.py
@@ -16,7 +16,7 @@ if "Homepage.py" in locals():
 else:
     current_dir = Path.cwd()
 
-print(os.listdir())
+print(os.listdir("."))
 
 #---load assets---
 img=Image.open(current_dir /"Images" / "Layout_1.PNG")
