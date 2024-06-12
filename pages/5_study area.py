e384a52cc67469bbe037a9bd22a08aac24d7dc65
David Rukavina <drukavina@rgn.hr>
2024-06-12 16:36:47 +0200

    test21

--- pages/5_study area.py
+++ pages/5_study area.py
@@ -5,7 +5,7 @@ import webbrowser
 import requests
 import os
 
-
+st.title("Adraitic foreland basin")
 
 st.write("Thrust belts bounded basin: to the west by the Apennine thrust belt and to the east by the Dinarides-Albanides thrust-belt. During the Cenozoic the development of the Alpine and Dinaric thrust belts provided a huge sediments influx in the basin and the deposition of thick clastic sequences.")
 st.write("Adriatic foreland map:")
