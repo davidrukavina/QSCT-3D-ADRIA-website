1edc616c42754d223318d5fec6bfc66e93cefbae
davidrukavina <rukavina.david1@gmail.com>
2024-11-08 13:29:45 +0100

    update

--- pages/4_resources.py
+++ pages/4_resources.py
@@ -7,12 +7,17 @@ if "homepage.py" in locals():
 else:
     current_dir = Path.cwd()
 
+img=Image.open(current_dir /"Images" / "azu.jpg")
+img2=Image.open(current_dir /"Images" / "tgs.jpg")
+print(current_dir / "Images" / "azu.jpg")
+
 st.title("Data")
 st.write("Project is based on analysis and interpretation of offshore 2D seismic and well data. Seismic data represents the core of the interest since it gives the chance to understand halokinetic structures, faults, depositional surfaces and bodies in subsurface.")
 st.write("#")
 st.write("This project is supported by the Croatian Hydrocarbon Agency and TGS , who provided the seismic and well data from the central and southern Adriatic Sea.")
-img=Image.open(current_dir /"Images" / "azu.jpg")
-img=Image.open(current_dir /"Images" / "tgs.jpg")
+st.image(img)
+st.image(img2)
+
 
 st.write("---")
 
